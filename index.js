a8e669348fcacbe84a816a86e232bf2dec7575ed
Tobias Polly <t4life120@gmail.com>
2023-06-25 09:17:40 +0200

    add get, post & delete /articles

--- index.js
+++ index.js
@@ -11,21 +11,55 @@ mongoose.connect(`${process.env.MONGO_URI}/wikiDB`);
 
 // setup bodyparser and ejs
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
 
 
-
-
-
-
-
-
-
+app.route("/articles")
+    .get((req, res) => {
+        Article.find()
+        .then(articles => {
+            res.send(articles);
+        })
+        .catch(err => {
+            console.log(err);
+            res.send("There was an error handling the request.\n" + err);
+        });
+    })
+    .post((req, res) => {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        
+        newArticle.save()
+            .then(info => {
+                res.send("200 -> Status ok!\n" + info);
+            })
+            .catch(error => {
+                res.send("404\n" + error);
+            })
+    })
+    .delete((req, res) => {
+        Article.deleteMany({})
+            .then(() => {
+                res.send("Successfully deleted all entries");
+            })
+            .catch(error => {
+                res.send(error);
+            })
+    })
 
 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}, localhost:${PORT} or http://localhost:${PORT}/`);
+    console.log(`Server is running on port ${PORT}, http://localhost:${PORT}/`);
 });
